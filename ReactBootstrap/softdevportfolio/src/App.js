0d0d38650ce629f15feff81137a47b0a570a6dcf
Muniramouly <muniramouly25@gmail.com>
2021-07-29 23:30:21 +0600

    react

--- ReactBootstrap/softdevportfolio/src/App.js
+++ ReactBootstrap/softdevportfolio/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TopBanner from './components/topBanner/TopBanner';
 import Services from './components/Services/Services';
 import TopNavigation from './components/TopNevigation/TopNavigation';
+import Analysis from './components/analysis/Analysis';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <TopNavigation></TopNavigation>
       <TopBanner></TopBanner>
       <Services></Services>
+      <Analysis></Analysis>
     </div>
   );
 }
