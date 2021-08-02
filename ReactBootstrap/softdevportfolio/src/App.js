b2102946f395c42e693837c31ebabc317266569d
ShovhanaSweety <sweetycse.engr@gmail.com>
2021-08-02 16:28:29 +0600

    Home

--- ReactBootstrap/softdevportfolio/src/App.js
+++ ReactBootstrap/softdevportfolio/src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TopBanner from './components/topBanner/TopBanner';
-import Services from './components/Services/Services';
-import TopNavigation from './components/TopNevigation/TopNavigation';
-import Analysis from './components/analysis/Analysis';
-import Summary from './components/Summary/Summary';
-import RecentProjets from './components/ReacentProjects/RecentProjets';
-import Courses from './components/Courses/Courses';
-import Vedio from './components/vedio/Vedio';
-import ClientReview from './components/ClientReview/ClientReview';
-import Footer from './components/footer/Footer';
-
+import HomePage from './pages/HomePage/HomePage';
 function App() {
   return (
     <div>
-      <TopNavigation></TopNavigation>
-      <TopBanner></TopBanner>
-      <Services></Services>
-      <Analysis></Analysis>
-      <Summary></Summary>
-      <RecentProjets></RecentProjets>
-      <Courses></Courses>
-      <Vedio></Vedio>
-      <ClientReview></ClientReview>
-      <Footer></Footer>
+      
+     <HomePage></HomePage>
       
     </div>
   );
